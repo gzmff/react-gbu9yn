72daf3f5053b2a8ed99ce3f7714e3a5f1b5b0436
gzmff <46191515+gzmff@users.noreply.github.com>
2022-10-27 10:34:54 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from "react";
-import "./style.css";
+import React, { useRef } from 'react';
+import './style.css';
 
 export default function App() {
+  const timeRef = useRef();
+  const time = useRef();
+  useEffect(() => {
+    timeRef.current = setInterval(() => {
+      if (time.current && time.current > 0) {
+        time.current -= 1;
+      } else {
+        time.current = 10;
+      }
+    });
+  }, []);
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
