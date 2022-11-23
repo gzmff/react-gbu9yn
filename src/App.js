5d6893024d748df51d9bfe716b4bebf8e777611c
gzmff <46191515+gzmff@users.noreply.github.com>
2022-11-23 21:51:10 +0800

    1 file modified

--- src/App.js
+++ src/App.js
@@ -5,6 +5,42 @@ export default function App() {
   const timeRef = useRef();
   const time = useRef();
   const [num, setNum] = useState(0);
+  class Foo {
+    static named = '_island';
+    #age;
+    constructor(name, age) {
+      this.name = name;
+      this.#age = age;
+    }
+    getage() {
+      return this.#age;
+    }
+  }
+  const dd = new Foo('sss', 19);
+  console.log(Foo.age, dd.age, dd.getage(), Foo.named, dd.named);
+  //undefined,undefined,19_island,undefined
+  const Singleton = function (name) {
+    this.name = name;
+  };
+  Singleton.instance = null;
+  console.log(Singleton.instance);
+  Singleton.prototype.getName = function () {
+    alert(this.name);
+  };
+  Singleton.getInstance = function (name) {
+    console.log(this.instance, 'ddd');
+    if (!this.instance) {
+      this.instance = new Singleton(name);
+    }
+    return this.instance;
+  };
+  const a = Singleton.getInstance('a');
+  const b = Singleton.getInstance('b');
+  // a.getName();
+  // b.getName();
+  useEffect(() => {
+    console.log(num);
+  }, [num]);
   useEffect(() => {
     timeRef.current = setInterval(() => {
       if (time.current && time.current > 0) {
@@ -18,9 +54,14 @@ export default function App() {
       }
     }, 1000);
   }, []);
+  const click = () => {
+    for (let i = 0; i < 100; i++) {
+      setNum((num) => num + 1);
+    }
+  };
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
+      <h1 onClick={click}>{num}</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
   );
